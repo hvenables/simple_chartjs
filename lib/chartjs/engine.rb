d8cc6395324ada5669384bfd328f9ab846a978a4
Harry Venables <harry@tagadab.com>
2017-12-19 11:21:13 +0000

    Removed adding simple_chartjs to precompile as it should be included in the application js

--- lib/chartjs/engine.rb
+++ lib/chartjs/engine.rb
@@ -1,13 +1,5 @@
 module Chartjs
   class Engine < ::Rails::Engine
-    initializer 'precompile', group: :all do |app|
-      if defined?(Sprockets) && Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("4.0.0.beta1")
-        app.config.assets.precompile << 'simple_chartjs.js'
-      else
-        app.config.assets.precompile << proc { |path| path == 'simple_chartjs.js' }
-      end
-    end
-
     initializer 'helper' do
       ActiveSupport.on_load(:action_view) do
         include ChartHelper
